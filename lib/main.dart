9899fb99a920c7586ba8d02b5978bc337ae004b5
Ocelot <atenziono@gmail.com>
2021-12-14 15:17:14 +0330

    Some code cleanup Google sign in button has been added provider attached to the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
-import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
+import 'package:world_time/services/GoogleSignInProvider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => Loading(),
-      '/home': (context) => Home(),
-      '/location': (context) => ChooseLocation(),
-    },
+  runApp(ChangeNotifierProvider(
+    create: (context) => GoogleSingInProvider(),
+    child: MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
+    ),
   ));
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:world_time/services/GoogleSignInProvider.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -18,68 +21,102 @@ class _HomeState extends State<Home> {
     String bgImage = data['isDayTime'] ? 'assets/day.png' : 'assets/night.png';
     Color? bgColor = data['isDayTime'] ? Colors.blue : Colors.indigo[800];
     return Scaffold(
-        backgroundColor: bgColor,
-        body: SafeArea(
-          child: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(bgImage), fit: BoxFit.cover)),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-              child: Column(
-                children: [
-                  TextButton.icon(
-                      onPressed: () async {
-                        dynamic result =
-                            await Navigator.pushNamed(context, '/location');
-                        setState(() {
-                          data = {
-                            'time': result['time'],
-                            'location': result['location'],
-                            'flag': result['flag'],
-                            'isDayTime': result['isDayTime'],
-                          };
-                        });
-                      },
-                      icon: Icon(
-                        Icons.edit_location,
-                        color: Colors.grey[300],
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(bgImage), fit: BoxFit.cover)),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            child: Column(
+              children: [
+                TextButton.icon(
+                    onPressed: () async {
+                      dynamic result =
+                          await Navigator.pushNamed(context, '/location');
+                      setState(() {
+                        data = {
+                          'time': result['time'],
+                          'location': result['location'],
+                          'flag': result['flag'],
+                          'isDayTime': result['isDayTime'],
+                        };
+                      });
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.grey[300],
+                    ),
+                    label: Text(
+                      'Edit location',
+                      style: TextStyle(color: Colors.grey[300]),
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                          fontSize: 28, letterSpacing: 2, color: Colors.white),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['time'],
+                      style: TextStyle(fontSize: 66, color: Colors.white),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50, vertical: 0),
+                        child: TextButton.icon(
+                            onPressed: () {
+                              final provider =
+                                  Provider.of<GoogleSingInProvider>(context,
+                                      listen: false);
+                              provider.googleLogin();
+                            },
+                            icon: Icon(
+                              FontAwesomeIcons.google,
+                              color: Colors.white,
+                            ),
+                            label: Text(
+                              'Google Sing in',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 19),
+                            ),
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.red,
+                                minimumSize: Size.fromHeight(60))),
                       ),
-                      label: Text(
-                        'Edit location',
-                        style: TextStyle(color: Colors.grey[300]),
-                      )),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        data['location'],
-                        style: TextStyle(
-                            fontSize: 28,
-                            letterSpacing: 2,
-                            color: Colors.white),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        data['time'],
-                        style: TextStyle(fontSize: 66, color: Colors.white),
-                      )
-                    ],
-                  )
-                ],
-              ),
+                    )
+                  ],
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
