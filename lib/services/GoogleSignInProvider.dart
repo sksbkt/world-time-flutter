4e6bfb91693dd08f7b85d185379a7b7bca2bad07
Ocelot <atenziono@gmail.com>
2021-12-14 15:17:14 +0330

    GoogleSingInProvider class has been added Google sign in button has been added provider attached to the main *** some code clean ups

---
+++ lib/services/GoogleSignInProvider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class GoogleSingInProvider extends ChangeNotifier {
+  final googleSingIn = GoogleSignIn();
+  GoogleSignInAccount? _user;
+  GoogleSignInAccount get user => _user!;
+  Future googleLogin() async {
+    final googleUser = await googleSingIn.signIn();
+    if (googleUser == null) return;
+    _user = googleUser;
+    final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+    await FirebaseAuth.instance.signInWithCredential(credential);
+    notifyListeners();
+  }
+}
