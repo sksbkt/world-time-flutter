4d9b682d2dc748205f1daa47418f0a230bbe05c6
Ocelot <atenziono@gmail.com>
2022-01-16 18:16:38 +0330

    More options added to Settings.dart TODO: attach 24 hours option to switch

--- lib/pages/Settings.dart
+++ lib/pages/Settings.dart
@@ -13,46 +13,54 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
+    final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
     final proTime = Provider.of<TimeUpdater>(context, listen: false);
-    final String selectedDateFormat = proTime.dateFormat;
+    // final String selectedDateFormat = proTime.dateFormat;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
       ),
       body: Container(
-          child: ListView(
-        children: [
-          ListTile(
-            title: Text('Theme'),
-            trailing: Switch.adaptive(
-                value: themeProvider.isDarkmode,
-                onChanged: (value) {
-                  themeProvider.toggleThemeMode(value);
-                }),
-          ),
-          ListTile(
-              title: Text('Theme'),
-              trailing: Consumer(
-                builder: (context, TimeUpdater, child) =>
-                    DropdownButton<String>(
-                  value: proTime.dateFormat,
-                  items: <String>['jm', 'jms'].map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                      onTap: () {
-                        proTime.changeDateformat(value);
-                      },
-                    );
-                  }).toList(),
-                  onChanged: (_) {
-                    setState(() {});
-                  },
-                ),
-              )),
-        ],
+          child: Consumer2<ThemeProvider, TimeUpdater>(
+        builder: (context, ThemeProvider, TimeUpdater, child) => ListView(
+          children: [
+            ListTile(
+              title: Text('Dark mode'),
+              trailing: Switch.adaptive(
+                  value: themeProvider.isDarkmode,
+                  onChanged: (value) {
+                    themeProvider.toggleThemeMode(value);
+                  }),
+            ),
+            ListTile(
+              title: Text('Time format'),
+              trailing: DropdownButton<String>(
+                value: proTime.dateFormat,
+                items: <String>['jm', 'jms'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                    onTap: () {
+                      proTime.changeDateformat(value);
+                    },
+                  );
+                }).toList(),
+                onChanged: (_) {
+                  // setState(() {});
+                },
+              ),
+            ),
+            ListTile(
+              title: Text('24'),
+              trailing: Switch.adaptive(
+                  value: proTime.hoursFormat24,
+                  onChanged: (value) {
+                    proTime.toggleHoursFormat24(value);
+                  }),
+            ),
+          ],
+        ),
       )),
     );
   }
--- lib/services/TimeProvider.dart
+++ lib/services/TimeProvider.dart
@@ -5,20 +5,25 @@ import 'package:intl/intl.dart';
 
 class TimeUpdater with ChangeNotifier {
   DateTime time = DateTime.now().toUtc();
+
+  bool hoursFormat24 = false;
+  bool showSeconds = true;
+
   String dateFormat = 'jm';
+
   DateFormat format = DateFormat.jm();
   Timer _timer = Timer.periodic(Duration.zero, (timer) {});
   void updateTime(int offHours, int offMins) {
     if (_timer.isActive) _timer.cancel();
     time =
         DateTime.now().toUtc().add(Duration(hours: offHours, minutes: offMins));
-    print(offHours.toString() + ' ' + offMins.toString());
+    // print(offHours.toString() + ' ' + offMins.toString());
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      print(_timer.tick);
+      // print(_timer.tick);
       time = DateTime.now()
           .toUtc()
           .add(Duration(hours: offHours, minutes: offMins));
-      print(time);
+      // print(time);
       notifyListeners();
     });
   }
@@ -26,20 +31,36 @@ class TimeUpdater with ChangeNotifier {
   void changeDateformat(String input) {
     switch (input) {
       case 'jm':
-        dateFormat = input;
         format = DateFormat.jm();
         break;
       case 'jms':
-        dateFormat = input;
+        format = DateFormat.jms();
+        break;
+      case 'Hm':
         format = DateFormat.jms();
         break;
     }
+    dateFormat = input;
+    notifyListeners();
     // print(format.format(time).toString());
     // print(dateFormat);
   }
 
   String getTime() {
-    print(format.format(time));
-    return format.format(time);
+    // print(format.format(time));
+    return DateFormat(getHour() + 'm' + getSeconds()).format(time);
+  }
+
+  void toggleHoursFormat24(bool value) {
+    hoursFormat24 = value;
+    notifyListeners();
+  }
+
+  String getHour() {
+    return hoursFormat24 ? 'H' : 'j';
+  }
+
+  String getSeconds() {
+    return showSeconds ? 's' : '';
   }
 }
